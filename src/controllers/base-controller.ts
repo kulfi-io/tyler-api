ef366bb52abd7ca3b42d6decef427358df11521e
ashish chatterjee <ashish@kulfi.io>
2019-08-03 20:39:51 -0700

    fixed decrypt error

--- src/controllers/base-controller.ts
+++ src/controllers/base-controller.ts
@@ -48,9 +48,15 @@ export  class BaseController {
     }
 
     protected decryptData(data: string): string {
-        var _data = crypto.AES.decrypt(data, config.transportSecret);
-        var _plaintext = _data.toString(crypto.enc.Utf8);
-        return this.isProd ? _plaintext : data;
+
+        if(this.isProd) {
+            var _data = crypto.AES.decrypt(data, config.transportSecret);
+            var _plaintext = _data.toString(crypto.enc.Utf8);
+            return _plaintext;
+        }
+
+        return data;
+       
     }
 
     protected mongoIdObject(data: string) {
